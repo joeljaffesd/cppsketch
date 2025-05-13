a248fb5379307180aaa7bf2e64c70b37192ab975
Joel Jaffe <joel@jaffesd.com>
2025-05-12 21:09:56 -0700

    Updated em++ flag in `examples/audioweb`

--- examples/audioweb/buildWeb.sh
+++ examples/audioweb/buildWeb.sh
@@ -2,4 +2,4 @@ em++ emscripten/em.cpp -O3 -I../../src/ -I../../ext/rtaudio -I. -o emscripten/em
 -s WASM=1 \
 -s ALLOW_MEMORY_GROWTH=1 \
 -s EXPORTED_FUNCTIONS="['_getSamples', '_update', '_setParameter', '_malloc', '_free']" \
--s EXTRA_EXPORTED_RUNTIME_METHODS="['cwrap','ccall']"
+-s EXPORTED_RUNTIME_METHODS="['cwrap','ccall']"
